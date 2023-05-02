5585d8c592aa955e5a8468442276a368669d134f
akashbelekar2428 <akashbelekar108@gmail.com>
2023-05-02 16:45:01 +0530

    Auth

---
+++ Example/Pods/Target Support Files/Authentications/Authentications-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Authentications : NSObject
+@end
+@implementation PodsDummy_Authentications
+@end
---
+++ Example/Pods/Target Support Files/Pods-Authentications_Tests/Pods-Authentications_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Authentications_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Authentications_Tests
+@end
